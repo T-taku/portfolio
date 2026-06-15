ff7c18bae3630c57fbf3f3e544264df72fc682c8
Takuma Tateishi <takutate.0427@gmail.com>
2026-06-15 19:32:03 +0900

    style: remove left padding from footer to center layout

--- app/components/_ui/Footer.tsx
+++ app/components/_ui/Footer.tsx
@@ -6,7 +6,7 @@ import { SiZenn } from "react-icons/si";
 export default function Footer() {
   return (
     <>
-      <footer className="flex w-full h-[260px] items-center justify-center bg-[#252525] text-white lg:pl-[248px]">
+      <footer className="flex w-full h-[260px] items-center justify-center bg-[#252525] text-white">
         <div className="flex w-full max-w-[1200px] justify-between items-center px-[70px]">
           <div className="flex flex-col gap-[10px]">
             <span className="text-[36px] font-medium tracking-[5%]">Takuma Tateishi</span>
